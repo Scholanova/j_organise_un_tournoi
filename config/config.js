5841a5d84322cb3a0c15ab1eabb5fe0c17f2c388
JoKing94 <joseph94.king@gmail.com>
2020-06-30 15:37:51 +0200

    ajout de la migration et du Procfile

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: 'postgres',
     password: 'postgres',
-    database: 'scholanova_express_juin_development',
+    database: 'myplayoff_development',
     host: '127.0.0.1',
     dialect: 'postgres',
     logging: (msg) => console.log('[DATABASE]', msg)
@@ -10,7 +10,7 @@ module.exports = {
   test: {
     username: 'postgres',
     password: 'postgres',
-    database: 'scholanova_express_juin_test',
+    database: 'myplayoff_test',
     host: '127.0.0.1',
     dialect: 'postgres',
     logging: false
@@ -18,7 +18,7 @@ module.exports = {
   production: {
     username: 'postgres',
     password: 'postgres',
-    database: 'scholanova_express_juin_production',
+    database: 'myplayoff_production',
     host: '127.0.0.1',
     dialect: 'postgres',
     logging: (msg) => console.log('[DATABASE]', msg)
