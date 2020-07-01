412d9b81d0333be088993fc2bfc200071266955d
JoKing94 <joseph94.king@yahoo.com>
2020-07-01 13:18:01 +0200

    ajout des modifs de benoit

--- db/migrations/20200630125149-poc_bdd_scalingo.js
+++ db/migrations/20200630125149-poc_bdd_scalingo.js
@@ -9,11 +9,7 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-<<<<<<< HEAD
-   return queryInterface.createTable('tournois', 
-=======
    return queryInterface.createTable('tournoi', 
->>>>>>> 436690aa71ca9e6064d3b3ebe8e77a5780a9e869
    {
     id: {
       allowNull: false,
@@ -23,17 +19,6 @@ module.exports = {
     },
       name: {
       type: Sequelize.STRING
-<<<<<<< HEAD
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-=======
->>>>>>> 436690aa71ca9e6064d3b3ebe8e77a5780a9e869
     }
    }
    );
@@ -47,9 +32,5 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-<<<<<<< HEAD
-   return queryInterface.dropTable('tournois');
-=======
->>>>>>> 436690aa71ca9e6064d3b3ebe8e77a5780a9e869
   }
 };
