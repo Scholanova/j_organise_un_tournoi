4ef8fccbbfadb644099e23b349d89362b567f8ef
Namelistone <namelistone@gmail.com>
2020-07-01 15:37:43 +0200

    REFRESH SEED + MIGRATION

--- db/migrations/20200701095958-create-match.js
+++ db/migrations/20200701095958-create-match.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Matches', {
+    return queryInterface.createTable('Matchs', {
       id: {
         allowNull: false,
         autoIncrement: true,
