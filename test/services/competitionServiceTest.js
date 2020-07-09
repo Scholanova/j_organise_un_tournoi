a1b5d9c99e5124889d302a19bb82fa65a9b9ef58
Namelistone <namelistone@gmail.com>
2020-07-09 14:16:55 +0200

    finish test service

--- test/services/competitionServiceTest.js
+++ test/services/competitionServiceTest.js
@@ -40,10 +40,13 @@ describe('competitionService', () => {
         beforeEach(() => {
             sinon.stub(competitionRepository, 'listAll')
         })
-        context('when there is competition', () => {
+        context('when there is one competition', () => {
+
+            let competition
+
             beforeEach(()=>{
                 // given
-                const competition =
+                competition =
                     {
                         id: 42000,
                         name_organisateur: 'Benoit',
@@ -52,70 +55,25 @@ describe('competitionService', () => {
                         status: true,
                         vainqueur: 42001
                     }
-
+                
                 competitionRepository.listAll.resolves([competition])
                 // when
                 
-                competitionListAllPromise = competitionService.findAll()
+                competitionFindAllPromise = competitionService.findAll()
                 
             })
             it('should call the competition Repository', async () => {
                 // then
-                await competitionListAllPromise.catch(() => {})
-                expect(competitionRepository.listAll).to.have.been.called
+                await competitionFindAllPromise.catch(() => {})
+                expect(competitionRepository.listAll).to.have.been.calledWith(undefined)
             })
             it('should result to an array', async () => {
                 // then
-                await competitionListAllPromise.catch(() => {})
+                const returnCompetitions = await competitionFindAllPromise.catch(() => {})
 
-                expect(competitionRepository.listAll).to.eventually.deep.equal([competition])
+                expect(returnCompetitions).to.deep.equal([competition])
             })
             
-        })
-        context('when the competition number of participant is not a number pair', () => {
-            beforeEach(()=>{
-                // given
-                // when
-                result = { name_organisateur: 'Coupe du monde 2022', name: 'Equipe 1', nb_participant: 1,status:false }
-
-            })
-            it('should not call the competition Repository', async () => {
-                // then
-                await result.catch(() => {})
-                expect(competitionRepository.listAll()).to.not.have.been.called
-            })
-            it('should reject with a ValidationError error about unsupported number of participant', () => {
-                // then
-                const expectedErrorDetails = [{
-                    message: '"number of participant" must be one of [french, english]',
-                    path: ['number of participant'],
-                    type: 'any.only',
-                    context: { label: 'nb_participant', key: 'nb_participant', valids: [2, 4,6,8,10,16,16], 'value': 1 }
-                }]
-                return expect(result)
-                    .to.eventually.be.rejectedWith(Joi.ValidationError)
-                    .with.deep.property('details', expectedErrorDetails)
-            })
-        })
-        context('when the competition number of participant is  a number pair', () => {
-            beforeEach(()=>{
-                // given
-                result = { name_organisateur: 'Coupe du monde 2022', name: 'Equipe 1', nb_participant: 8,status:false }
-                Competition = competitionService.create(result)
-                competitionRepository.listAll().resolves([Competition])
-                let nb_participant = 'french'
-                // when
-                result = competitionService.findAll({nb_participant : nb_participant})
-            })
-            it('should call the competition Repository with the number of participation pair', async () => {
-                // then
-                await result.catch(() => {})
-                expect(competitionRepository.listAll()).to.have.been.calledWith(nb_participant)
-            })
-            it('should resolve with the competition listed from reprository', () => {
-                // then
-                return expect(result).to.eventually.deep.equal([Competition])
-            })
         })
     })
 
